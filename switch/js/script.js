f25368cd5f89efd2302c18b555850877f0c7462d
Anderson Jorge <anderson.jorge33@gmail.com>
2021-02-08 18:47:21 -0300

    aquivos da aula de switch

---
+++ switch/js/script.js
@@ -0,0 +1,16 @@
+x = prompt("qual é o número?")
+
+switch(x){
+    case "0":
+        alert("X é 0")
+        break
+    case "1":
+        alert("X é 1")
+        break
+    case "2":
+        alert("X é 2")
+        break
+    default:
+        alert("ninguém foi satisfeito!")
+        break
+}
