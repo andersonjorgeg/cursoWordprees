31e5f6a0ecad4818173081080395a1bb15c472f2
Anderson Jorge <anderson.jorge33@gmail.com>
2021-02-08 18:28:25 -0300

    arquivos da aula de loop

---
+++ loop/js/script.js
@@ -0,0 +1,20 @@
+/*let x = 0;
+
+ document.write("Iniciando o loop... <br>")
+
+while(x < 10){
+
+    document.write("Número: " + x + "<br>")
+
+    x++
+}
+
+document.write("Finalizando o loop... <br>") */
+
+
+for(let i = 0; i < 10; i++){
+    document.write(`Número: ${i} <br>`)
+}
+
+
+
