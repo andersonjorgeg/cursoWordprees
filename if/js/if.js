e6ec24f410d51d3e9b9da8f75a443ff4615f38fe
Anderson Jorge <anderson.jorge33@gmail.com>
2021-02-08 15:15:25 -0300

    arquivos da aula de if

---
+++ if/js/if.js
@@ -0,0 +1,16 @@
+let nome = "anderson"
+
+if(nome == "anderson"){
+    console.log("Esse é o meu nome!")
+} else {
+    console.log("Esse não é meu nome!")
+}
+
+if(nome == "junior"){
+    console.log("Esse é o meu nome!")
+} else {
+    console.log("Esse não é meu nome!")
+}
+
+
+
---
+++ if/js/script.js
@@ -0,0 +1,9 @@
+function testeVerificador(){
+    let x = prompt("Diga um número")
+
+    if(x > 10){
+        alert("É maior que 10!!!")
+    } else {
+        alert("NÃO é maior que 10!")
+    }
+}
